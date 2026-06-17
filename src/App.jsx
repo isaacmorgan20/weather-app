ce777a2dcc7ffb3c32dbd452b56143ac364ed3c7
Isaac-Morgan <morganisaackojo5547@gmail.com>
2026-06-17 08:20:48 +0000

    Added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Home from './Pages/Home'
-import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <section>
       <Home />
-      <NavBar />
     </section>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Hero from '../Components/Hero';
+import Navbar from '../Components/NavBar';
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleNavSearch = (city) => {
+    // Update search term to trigger Hero search.
+    setSearchTerm(city);
+  };
+
+  // Reset search term after Hero processes it (optional, improves repeat searches).
+  const handleHeroSearchProcessed = () => {
+    setSearchTerm('');
+  };
+
   return (
     <section>
-        
+      <Navbar onSearch={handleNavSearch} />
+      <Hero searchTerm={searchTerm} onSearchProcessed={handleHeroSearchProcessed} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
